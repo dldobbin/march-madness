d14cc6a0bffd04f4206b45b4e564fa1dbcbbde30
daniel <dldobbin@gmail.com>
2018-04-19 21:51:52 -0500

    changed import statements to agree with the renaming

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-import ncaa_data
+import comparator
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def _zip(*args, **kwargs):
 @app.route('/', methods=['GET'])
 def index():
 	year = request.args['year'] if 'year' in request.args else '2018'
-	tourney = ncaa_data.NCAA(year)
+	tourney = comparator.NCAA(year)
 	prediction = tourney.chalk() if 'chalk' in request.args else tourney.sim()
 	return render_template("index.html", year=int(year), actual=tourney.results, prediction=prediction['outcome'], score=prediction['score'])
 
